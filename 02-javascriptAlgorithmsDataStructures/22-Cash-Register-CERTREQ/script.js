a38e3dddd063b19baee0e6060b951e4aa9366a67
kirigaine <tholen2147@gmail.com>
2024-08-07 18:56:10 -0500

    Essentially finished logic for calculating leftover and register status

--- 02-javascriptAlgorithmsDataStructures/22-Cash-Register-CERTREQ/script.js
+++ 02-javascriptAlgorithmsDataStructures/22-Cash-Register-CERTREQ/script.js
@@ -1,4 +1,6 @@
 let price = 1.87;
+// Total cash in drawer: $335.41
+// Max dispense @ $337.28 - $337.29 tipping point should empty register and fail 
 let cid = [
   ['PENNY', 1.01],
   ['NICKEL', 2.05],
@@ -25,12 +27,12 @@ const cvalue = [
 const changeDue = document.getElementById("change-due");
 const btnPurchase = document.getElementById("purchase-btn");
 const cashEle = document.getElementById("cash");
+let dStatus = "OPEN";
 
 
 const checkCash = () => {
   let cash = parseFloat(cashEle.value);
 
-  //console.log(`cash: ${cash} price: ${price}`);
 
   if (cash < price) alert("Customer does not have enough money to purchase the item");
   else if (cash === price) changeDue.textContent = "No change due - customer paid with exact cash";
@@ -38,24 +40,30 @@ const checkCash = () => {
   else{
 
     let leftover = parseFloat(cash-price).toFixed(2);
+    let i=cvalue.length-1;
 
-    // TODO: Change this logic where it iterates until it cannot instead of using i++
-
-    for(let i=cvalue.length-1;i>=0;i--){
-      if (parseFloat(leftover - cvalue[i][1]).toFixed(2) >=0 && cid[i][1] > 0){
-        //console.log(`leftover: ${leftover} cvalue: ${cvalue[i][1]}`)
-        leftover = parseFloat(leftover - cvalue[i][1]).toFixed(2);
-        //console.log(`leftover: ${leftover} cvalue: ${cvalue[i][1]}`)
-        cid[i][1] = parseFloat(cid[i][1] - cvalue[i][1]).toFixed(2);
-        // DEFINITELY BAD ITERATION CODE, change later
-        i++;
-      }
+    while(i>=0){
+      let typeCount = 0;
+      if (parseFloat(leftover - cvalue[i][1]).toFixed(2) >=0 && cid[i][1] > 0 ){
+          leftover = parseFloat(leftover - cvalue[i][1]).toFixed(2);
+          cid[i][1] = parseFloat(cid[i][1] - cvalue[i][1]).toFixed(2);
+          typeCount++;
+          //console.log(`leftover: ${leftover} cvalue: ${cvalue[i][1]}`);
+          }
       else{
-         console.log(`cash: ${cash} price: ${price} leftover: ${leftover}
-        // `);
-        // console.log(`${i} CID: ${cid}`);
+        //337.28
+        const everyZero = cid.every((value)=> parseFloat(value[1]) === 0);
+        if(i===0 && parseFloat(leftover) !== 0 && everyZero) {
+        alert("insufficient funds"); dStatus = "INSUFFICIENT_FUNDS";}
+        else if(i===0 && parseFloat(leftover) === 0 && everyZero) {dStatus = "CLOSED";}
+        i--;
+//
+        // console.log(cid.every((key,value)=> cid[key] === 0));
+        console.log(`cash: ${cash} price: ${price} leftover: ${leftover} status: ${dStatus}`);
       }
-    }
+
+}
+    
      console.log(`END CID: ${cid}`);
 
 
