ed114186224f7cd21b34e9d47ce34b6a7c93263b
kirigaine <tholen2147@gmail.com>
2024-08-14 18:58:49 -0500

    Added some CSS, changed layout, clarified alert

--- 02-javascriptAlgorithmsDataStructures/22-Cash-Register-CERTREQ/script.js
+++ 02-javascriptAlgorithmsDataStructures/22-Cash-Register-CERTREQ/script.js
@@ -1,4 +1,3 @@
-
 let price = 19.5
 
 // Total cash in drawer: $335.41
@@ -37,10 +36,15 @@ const STATUS = {
 const changeDue = document.getElementById("change-due");
 const btnPurchase = document.getElementById("purchase-btn");
 const cashEle = document.getElementById("cash");
+const changeLeft = document.getElementById("change-left");
+const thePrice = document.getElementById("price");
+
 let dStatus = STATUS.OPEN;
 
 const updateChange = (changeString) => { 
     changeDue.textContent = `Status: ${dStatus} ${changeString}`;
+    changeLeft.textContent = cid;
+    thePrice.textContent = `\$${price.toFixed(2)}`;
 }
 
 const checkCash = () => {
@@ -51,8 +55,14 @@ const checkCash = () => {
   if (cashRegex.test(cash)){
 
     // Determine if customer can afford or has exact cash, then no logic needed
-    if (cash < price) alert("Customer does not have enough money to purchase the item");
-    else if (cash === price) changeDue.textContent = "No change due - customer paid with exact cash";
+    if (cash < price) {
+      alert("Customer does not have enough money to purchase the item");
+      return;
+    }
+    else if (cash === price){
+      changeDue.textContent = "No change due - customer paid with exact cash";
+      return;
+    }
 
     else{
 
@@ -78,26 +88,32 @@ const checkCash = () => {
             changeDispensedValue += changeValue;
             }
 
-        // If we fail to subtract change, check if we're out of money
-        // Otherwise we go to next iteration
         else{
 
-          const everyZero = cid.every((value)=> parseFloat(value[1]) === 0);
-
-          if(i===0 && parseFloat(remainingDue) !== 0) {
-            dStatus = STATUS.INSUFFICIENT_FUNDS;
-            changeString = "";
-            changeDispensedValue = 0;
-          }
-
-          else if(i===0 && parseFloat(remainingDue) === 0 && everyZero) {dStatus = "CLOSED";}
+          const isDrawerEmpty = cid.every((value)=> parseFloat(value[1]) === 0);
           
-          
-
+          // If we dispensed any change for the iteration, add it to string builder
           if (changeDispensedValue !== 0){
-            changeString += `${changeKey}: \$${changeDispensedValue.toFixed(2)}`;
+            //const pEle = document.createElement("span");
+            //pEle.textContent = `${changeKey}: \$${changeDispensedValue.toFixed(2)}`;
+            //changeDue.appendChild(pEle);
+            changeString += `\n${changeKey}: \$${changeDispensedValue.toFixed(2)}`;
             changeDispensedValue=0;
           }
+
+          // Check and change register status 
+          if (i===0){
+            if(parseFloat(remainingDue) !== 0){
+              dStatus = STATUS.INSUFFICIENT_FUNDS;
+              changeString = "";
+              changeDispensedValue = 0;
+            }
+            else if(parseFloat(remainingDue) === 0 && isDrawerEmpty){
+              dStatus = STATUS.CLOSED;
+            }
+          }
+
+          // Get to this logic when no more can be subtracted, so iterate
           i--;
           
         }
@@ -108,8 +124,9 @@ const checkCash = () => {
 }
 }
 else{
-  alert("INVALID MONEY NUMBER");
+  alert("INVALID MONEY NUMBER - ONLY NUMERIC INPUT NO $, OPTIONAL 2 DECIMAL PLACES");
 }
 }
 
 btnPurchase.addEventListener("click",checkCash);
+
